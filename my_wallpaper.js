a94a74964bbf05c61bd5d1c4721b820b72389680
bissetemil1 <140354171+bissetemil1@users.noreply.github.com>
2023-07-24 12:01:52 +1200

    Update my_wallpaper.js
    attempt at snowman

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -21,4 +21,19 @@ function wallpaper_background() {
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   rect(40 ,40, rect_width, rect_height);
   rect(40 ,40, rect_width, rect_height);
+
+  strokeWeight(5);
+  line(70,35,130,35);
+
+  strokeWeight(3);
+  fill("#e1e2e8");//lightgrey
+  ellipse(100,57,45,40);//head
+  ellipse(100,100,50,45);//body
+  ellipse(100,155,70,65);//body base
+
+  strokeWeight(2);
+  stroke("#868ba3");// dark purpely/blue
+  fill("#868ba3");
+  ellipse(90,55,10,10);//right eye
+  ellipse(110,55,10,10);//left eye
 }
