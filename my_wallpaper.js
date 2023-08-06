2573a200d07cbc0135563b0444059a1ff0371669
bissetemil1 <140354171+bissetemil1@users.noreply.github.com>
2023-08-06 20:30:53 +1200

    Update my_wallpaper.js
    added another snowman...makes the grid look too busy tho

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -7,7 +7,7 @@ let rect_height = 20;
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -23,22 +23,22 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 //rect(40 ,40, rect_width, rect_height);
 //rect(40 ,40, rect_width, rect_height);
 
-var bowX = 35
-var bowY = 35
+var bowX = 120
+var bowY = 30
 var ribbionX = 30
 var ribbionY = 30
-var snowmanX = 120
+var snowmanX = 160
 var snowmanY = 110
 
  //ribbion
  stroke(1);
  stroke("#cc94d4");//darker pinkpurple
  fill("#e1bee6");//pinkpurple
- rect(ribbionX,ribbionY*0,10,200,);//y-ribbion
- rect(ribbionX+30,ribbionY*0,10,200,);//y-ribbion
- rect(ribbionX+60,ribbionY*0,10,200,);//y-ribbion
- rect(ribbionX+90,ribbionY*0,10,200,);//y-ribbion
- rect(ribbionX+120,ribbionY*0,10,200,);//y-ribbion
+ //rect(ribbionX,ribbionY*0,10,200,);//y-ribbion
+ //rect(ribbionX+30,ribbionY*0,10,200,);//y-ribbion
+ //rect(ribbionX+60,ribbionY*0,10,200,);//y-ribbion
+ //rect(ribbionX+90,ribbionY*0,10,200,);//y-ribbion
+ //rect(ribbionX+120,ribbionY*0,10,200,);//y-ribbion
 
 
  rect(ribbionX*0,ribbionY,200,10);//x-ribbion
@@ -48,9 +48,9 @@ var snowmanY = 110
  rect(ribbionX*0,ribbionY+120,200,10);//x-ribbion
 
  //bow
- //triangle(bowX+35,bowY-25,bowX+35,bowY+25,bowX,bowY);
- //triangle(bowX-33,bowY+25,bowX-33,bowY-25,bowX,bowY);
- //ellipse(bowX,bowY,30,30);
+ triangle(bowX+35,bowY-25,bowX+35,bowY+25,bowX,bowY);
+ triangle(bowX-33,bowY+25,bowX-33,bowY-25,bowX,bowY);
+ ellipse(bowX,bowY,30,30);
 
 //snowman
   strokeWeight(2);
@@ -88,8 +88,45 @@ var snowmanY = 110
   fill("#faa250");//orange
   triangle(snowmanX+1,snowmanY+8,snowmanX-10,snowmanY+10,snowmanX+2,snowmanY+11);
 
+
+  var SManX=40
+  var SManY=55
  
+ //snowman
+ strokeWeight(2);
+ stroke("#cce1e3");//lightblue
+ fill("#cce1e3");//lightblue
+ ellipse(SManX,SManY,50,45);//head
+ ellipse(SManX,SManY+55,70,65);//body base
+
+ //eyes
+ strokeWeight(1.5);
+ stroke("#868ba3");// dark purpely/blue
+ fill("#ffffff");//white
+ ellipse(SManX-10,SManY,10,10);//right eye
+ ellipse(SManX+10,SManY,10,10);//left eye
+ stroke("#868ba3");// dark purpely/blue
+ fill("#868ba3");
+ ellipse(SManX-10,SManY-3,3,4);//middle of eye
+ ellipse(SManX+10,SManY-3,3,4);//middle of eye
+
+ //hat
+ fill("#4b4661");
+ stroke("#4b4661");//greyblue
+ strokeWeight(3);
+ line(SManX-25,SManY-20,SManX+15,SManY-24);
  
+ beginShape();
+ vertex(SManX-30,SManY-46);
+ vertex(SManX+15,SManY-50);
+ vertex(SManX+5,SManY-25);
+ vertex(SManX-15,SManY-22);
+ endShape(CLOSE);
+ 
+ //nose
+ stroke("#faa250");//orange
+ fill("#faa250");//orange
+ triangle(SManX+1,SManY+8,SManX-10,SManY+10,SManX+2,SManY+11);
 
 
 }
